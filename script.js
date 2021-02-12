671a545d672fe39f6e97b5320a0b1048c1cf3dac
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-12 17:06:52 +0000

    Javascript changed

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.getElementById('length').addEventListener('click', area )
 
 // This function will grab the length and call it X, then square it
 function area () {
-  let x = document.getElementById('area').value
-  x = parseInt(x)
-  x = x ** 2
-  document.getElementById('result').innerHTML = "Your area is " + x
+  let side = document.getElementById('area').value
+  side = parseInt (side)
+  side = side ** 2
+  document.getElementById('result').innerHTML = 'Your area is ' + side
 }
