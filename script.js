e7c84d3d9188cea9797af469beb0d4b1f5ef090c
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-12 17:10:33 +0000

    parentheses change

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict'
 
 // This line will call the event for clicking the button and run the function
-document.getElementById('length').addEventListener('click', area )
+document.getElementById('length').addEventListener('click', area)
 
 // This function will grab the length and call it X, then square it
 function area () {
   let side = document.getElementById('area').value
-  side = parseInt (side)
+  side = parseInt(side)
   side = side ** 2
   document.getElementById('result').innerHTML = 'Your area is ' + side
 }
